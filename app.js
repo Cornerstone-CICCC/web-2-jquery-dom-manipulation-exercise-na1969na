602dfb380616d3c1820df6f8d29d2a2fcf16f2a6
na1969na <nakmtn07@gmail.com>
2024-11-26 11:26:54 -0800

    Done exercise

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
-// COMPLETE THE CODE
+$(function () {
+  $("#exercise1Button").on("click", function () {
+    $("#exercise1 ul li").css("color", "blue");
+  });
 
-$(function() {
-    // Exercise 1: Find all <li> elements within a <ul> and change their text color to blue
-    $("#exercise1Button").on('click', function() {
-      $("#exercise1 ul").
-    });
+  $("#exercise2Button").on("click", function () {
+    $("#exercise2 p").parent("div").css("background-color", "yellow");
+  });
 
-    // Exercise 2: Find the parent <div> of a paragraph and change its background color
-    $("#exercise2Button").on('click', function() {
-      $("#exercise2 p").
-    });
+  $("#exercise3Button").on("click", function () {
+    $("#exercise3 p").parents("div").css("background-color", "lightgreen");
+  });
 
-    // Exercise 3: Find all ancestor <div> elements of a paragraph and change their background color
-    $("#exercise3Button").on('click', function() {
-      $("#exercise3 p").
-    });
+  $("#exercise4Button").on("click", function () {
+    $(this).siblings("h3").nextAll("p").eq(1).hide();
+  });
 
-    // Exercise 4: From the `this` selector, find and hide only the second <p> sibling of the <h3> element. The `this` keyword refers to the clicked button
-    $("#exercise4Button").on('click', function() {
-      $(this).
-    });
+  $("#exercise5Button").on("click", function () {
+    $(this).closest("div").find("p").css("border", "1px solid black");
+  });
 
-    // Exercise 5: From the `this` selector, find all <p> elements within the <div> and add a border to them. The `this` keyword refers to the clicked button
-    $("#exercise5Button").on('click', function() {
-      $(this).
-    });
-
-    // Exercise 6: From the `this` selector, find and change the color of <span>Item 5</span>. The `this` keyword refers to the clicked button
-    $("#exercise6Button").on('click', function() {
-      $(this).
-    });
-})
+  $("#exercise6Button").on("click", function () {
+    $("ul").find("span:contains('Item 5')").css("color", "red");
+  });
+});
